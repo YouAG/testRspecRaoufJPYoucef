371430a8062051f82e0b2d5a76669d685aa03fe9
JPNK <jp.khadri@gmail.com>
2018-04-09 18:26:28 +0200

    bookinng

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+def title=(t)
+  @title = titleize(t)
+end 
+def title
+  @title
+end
+end
+Books = ["a", "an", "and", "over", "the", "in", "of"]
+def titleize(s)
+t = s.split.map { |x|
+ Books.include?(x) ? x : x.capitalize
+}
+t.first.capitalize!
+t.join(" ")
 end
