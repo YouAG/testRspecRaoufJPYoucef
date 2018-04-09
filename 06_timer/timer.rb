7a6143180cc2b365fedd9601e768252add10d92c
Raouf <Djekboub@gmail.com>
2018-04-09 18:29:35 +0200

    essaie sur exo timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
+
 class Timer
-  #write your code here
+	attr_accessor :seconds
+    
+	def initialize
+		@seconds = 0
+	end
+    
+	def time_string
+		heures = @seconds/3600
+		restant = @seconds%3600
+		sprintf("%02d:%02d:%02d", heures, restant/60, restant%60)
+	end
+
+
 end
